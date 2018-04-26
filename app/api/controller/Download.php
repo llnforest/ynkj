d321c0b317b23d2e49af28c43fd859fea726d80c
llnforest <1220343451@qq.com>
2018-04-26 14:58:52 +0800

    '修改完善2'

--- app/api/controller/Download.php
+++ app/api/controller/Download.php
@@ -21,7 +21,7 @@ class Download extends Controller {
         header("Content-Type: application/octet-stream");
         header("Accept-Ranges: bytes");
         header("Accept-Length: ".filesize($path_name));
-        header("Content-Disposition: attachment; filename='翼鸟宜居.ipa'");
+        header("Content-Disposition: attachment; filename=翼鸟宜居.ipa");
         echo fread($file,filesize($path_name));
         fclose($file);
     }
@@ -29,13 +29,23 @@ class Download extends Controller {
     //下载安卓
     public function downAndroid(){
         $path_name = $this->path.'/app/ynyj_android.apk';
-        $file=fopen($path_name,"r");
-        header("Content-Type: application/octet-stream");
-        header("Accept-Ranges: bytes");
-        header("Accept-Length: ".filesize($path_name));
-        header("Content-Disposition: attachment; filename='翼鸟宜居.apk'");
-        echo fread($file,filesize($path_name));
-        fclose($file);
+//        $file=fopen($path_name,"r");
+//        header("Content-Type: application/octet-stream");
+//        header("Accept-Ranges: bytes");
+//        header("Accept-Length: ".filesize($path_name));
+//        header("Content-Disposition: attachment; filename='翼鸟宜居.apk'");
+//        echo fread($file,filesize($path_name));
+//        fclose($file);
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=翼鸟宜居.apk');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($path_name));
+        readfile($path_name);
+        exit;
     }
 
 
